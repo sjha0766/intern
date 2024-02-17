847d045c9b9d1f1106b9ac6708e1663da0b51b51
sjha0766 <sjha0766@gmail.com>
2024-02-17 18:21:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var arr=['sandeep',1,2,3];
+
+const data={
+    name:'sandeep',
+    social:{
+        facebook:{
+            first:'sandeep',
+            second:'sourabh'
+        }
+    }
+};
+const {first,second}=data.social.facebook;
+
+
+console.log(first);
+console.log(second);
